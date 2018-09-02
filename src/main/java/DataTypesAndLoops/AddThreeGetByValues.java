8885b8ad67947909f9bb734b8febd6a86df24615
shriram2222 <shriram2222@gmail.com>
2018-09-02 10:38:14 +0100

    minor one

---
+++ src/main/java/DataTypesAndLoops/AddThreeGetByValues.java
@@ -0,0 +1,21 @@
+package DataTypesAndLoops;
+
+public class AddThreeGetByValues {
+
+    public static int firstOne(int a) {
+        return a;
+    }
+
+    public static int secondOne(int b) {
+        return b;
+    }
+
+    public static int thirdOne(int c) {
+        return c;
+    }
+
+    public static void main(String[] args){
+      int d=firstOne(22)+secondOne(2)+thirdOne(3);
+        System.out.println(d);
+    }
+}
---
+++ src/main/java/DataTypesAndLoops/Arguments.java
@@ -0,0 +1,11 @@
+package DataTypesAndLoops;
+
+public class Arguments  {
+    public static void main(String[] args){
+//        args[0]="1";
+//        args[1]="2";
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[1]);
+        }
+    }
+}
